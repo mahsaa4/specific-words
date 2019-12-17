ae456970e074b3437691110225a6935e922b012a
mahsa <mahsa.ahani4@gmail.com>
2019-12-17 12:06:26 +0330

    add changes

--- کلمات شاخص.py
+++ کلمات شاخص.py
@@ -1,10 +1,13 @@
+import re
 from collections import OrderedDict
-mylist = []
-mylist1 = []
-mydict = OrderedDict() 
-n = input().split(" ")
-mylist.append(n)
-for word in mylist:
-    for x in word:
-        mydict[x] = word.index(x)+1
-    print(mydict)
+SampleReport = input()
+pat = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
+sentences = pat.findall(SampleReport)
+count = 0
+for sentence in sentences:
+    splits = sentence.split()
+    for index, split in enumerate(splits):
+        count +=1
+        if split.istitle() and index != 0:
+            new_split = split.rstrip('.,')
+            print(count , ':' , new_split)
